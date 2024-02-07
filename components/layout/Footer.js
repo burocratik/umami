7f3e21798b807927631462c6ba156ea38560a295
jnrebelo <24824891+jnrebelo@users.noreply.github.com>
2024-02-07 16:57:30 +0000

    Update Footer.js

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
           values={{
             name: (
               <Link href={HOMEPAGE_URL}>
-                <b>Büro Analytics</b>
+                <b>Büro Analytics ds</b>
               </Link>
             ),
           }}
